b4909f774c852d863d680a28970d07d2a71bb4d3
Giovanna Grigolon <103059907+grigogi@users.noreply.github.com>
2024-12-03 21:08:06 +0100

    Update LCD.cpp

--- BE_Arduino_code/LCD.cpp
+++ BE_Arduino_code/LCD.cpp
@@ -2,24 +2,24 @@
 #include "LCD.h"
 
 LCD::LCD(int rs, int enable, int d4, int d5, int d6, int d7) : lcd(rs, enable, d4, d5, d6, d7) {
-    // Construtor da classe LCD, inicializa o LCD com os pinos fornecidos
+   
 }
 
 void LCD::Init() {
-    lcd.begin(16, 2);  // Inicializa o LCD com 16 colunas e 2 linhas
-    lcd.clear();       // Limpa a tela ao iniciar
+    lcd.begin(16, 2);  
+    lcd.clear();       
 }
 
 void LCD::SetTextF1(char* texto) {
-    TextF1 = texto;   // Define o texto a ser exibido no LCD
+    TextF1 = texto;   
 }
 
 void LCD::SetPosLCD(int num) {
-    PosLCD = num;     // Define a posição do cursor (coluna 0 a 15)
-    lcd.setCursor(PosLCD, 0);  // Move o cursor para a posição dada
+    PosLCD = num;    
+    lcd.setCursor(PosLCD, 0);  
 }
 
 void LCD::Refresh() {
-    lcd.clear();           // Limpa o display
-    lcd.print(TextF1);     // Exibe o texto no LCD
+    lcd.clear();           
+    lcd.print(TextF1);    
 }
