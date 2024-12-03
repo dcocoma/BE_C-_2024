04cbbc4d6c00c4c646d411f4a45f35c35bcceff9
Giovanna Grigolon <103059907+grigogi@users.noreply.github.com>
2024-12-03 21:08:37 +0100

    Update LED.cpp

--- BE_Arduino_code/LED.cpp
+++ BE_Arduino_code/LED.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <Arduino.h>
 using namespace std;
-class LED {
-private:
-    int pin;
-   
-public:
-    // Constructor
-    LED(int p) : pin(p) {}
+#include "LED.h"
+LED::LED(int p) : pin(p){}
 
-    void Init() {
-        pinMode(pin, OUTPUT); // set pin as an input
-        
-    }
 
-    void SetEtat(bool c){
-        if (c) {
+void LED::init() {
+    pinMode(pin, OUTPUT);  //distance by divisor
+}
+
+void LED::setEtat(bool c) {
+    if (c) {
          digitalWrite(pin, HIGH);
         else{
          digitalWrite(pin, LOW);
-         }
+         } 
+}
 };
