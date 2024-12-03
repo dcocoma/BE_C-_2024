a5607767eb79d8cac3d1d7e0379a14f9fcddfde3
Giovanna Grigolon <103059907+grigogi@users.noreply.github.com>
2024-12-03 21:09:44 +0100

    Update SenProx.cpp

--- BE_Arduino_code/SenProx.cpp
+++ BE_Arduino_code/SenProx.cpp
@@ -1,37 +1,13 @@
 #include <iostream>
+#include "SenProx.h"
 #include <Arduino.h>
 using namespace std;
-class SenProx {
-private:
-    int pin;
-    int distance; 
+SenProx::SenProx(int p) : pin(p), distance(0) {}
 
-public:
-    // Constructor
-    SenProx(int p) : pin(p), distance(0) {}
+void SenProx::init() {
+    pinMode(pin, INPUT_PULLUP); /
+bool SenProx::getDistance() {
+    distance = analogRead(pin);
+    return distance;
+}
 
-    void Init() {
-        pinMode(pin, INPUT); // set pin as an input
-        
-    }
-
-    int getDistance() {
-        distance = analogRead(pin);
-        return distance;
-    }
-};
-
-// main arduino
-
-//SensProx sensor(2); // Sensor conectado no pino 2
-
-//void setup() {
-//    Serial.begin(9600);
-//   sensor.Init(); // Inicializa o sensor
-//}
-
-//void loop() {
-//    int dist = sensor.getDistance();
-//   Serial.println("Distância: " + String(dist));
-//    delay(1000);
-//}
