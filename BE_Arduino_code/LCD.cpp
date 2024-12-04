214eb4c73757c2dda3fd70983e94390e389aa649
Giovanna Grigolon <103059907+grigogi@users.noreply.github.com>
2024-12-04 09:05:31 +0100

    Update LCD.cpp

--- BE_Arduino_code/LCD.cpp
+++ BE_Arduino_code/LCD.cpp
@@ -5,21 +5,21 @@ LCD::LCD(int rs, int enable, int d4, int d5, int d6, int d7) : lcd(rs, enable, d
    
 }
 
-void LCD::Init() {
+void LCD::init() {
     lcd.begin(16, 2);  
     lcd.clear();       
 }
 
-void LCD::SetTextF1(char* texto) {
+void LCD::setTextF1(char* texto) {
     TextF1 = texto;   
 }
 
-void LCD::SetPosLCD(int num) {
+void LCD::setPosLCD(int num) {
     PosLCD = num;    
     lcd.setCursor(PosLCD, 0);  
 }
 
-void LCD::Refresh() {
+void LCD::refresh() {
     lcd.clear();           
     lcd.print(TextF1);    
 }
